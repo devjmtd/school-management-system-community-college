2d37e9a72b8742eba3f5afdf01a2abbcfc0a5f80
Jose Marie Dimapilis <jtdimapillis@lessandra.com.ph>
2025-09-25 15:03:15 +0800

    fix: vite input again

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css',],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin-portal/theme.css',],
             refresh: true,
         }),
         tailwindcss(),
